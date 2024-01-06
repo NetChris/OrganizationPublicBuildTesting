c0d4a3719fa0d1b9f7802b6cb6b16d0371ffdc79
Chris Simmons <chris.simmons@gettyimages.com>
2024-01-05 16:05:37 -0800

    Unignore scratches

---
+++ scratches/scratch1-clean.ps1
@@ -0,0 +1,20 @@
+# Set the resource group name
+$ResourceGroup = "PublicBuildTesting"
+
+# Set the managed identity name
+$ManagedIdentityName = "GitHub-NetChris-PublicBuildTesting"
+
+# Set the GitHub repository name in the format: pauldotyu/osinfo
+$GitHubRepo = "NetChris/PublicBuildTesting"
+
+# Set the GitHub branch name
+$GitHubBranch = "building-and-testing-dotnet"
+
+"Deleting GitHub secrets ..."
+gh secret delete AZURE_CLIENT_ID
+gh secret delete AZURE_TENANT_ID
+gh secret delete AZURE_SUBSCRIPTION_ID
+gh secret delete ACR_LOGIN_SERVER
+
+"Deleting managed identity $ManagedIdentityName ..."
+az identity delete -n $ManagedIdentityName --resource-group $ResourceGroup
---
+++ scratches/scratch1.ps1
@@ -0,0 +1,69 @@
+# TODO - Set these in env
+$Subscription20210521 = XXX
+$SubscriptionMSDN = XXX
+$SubscriptionMain = XXX
+$AcrName = XXX
+$ManagedIdentityName = XXX
+
+# Set the GitHub repository name in the format: pauldotyu/osinfo
+$GitHubRepo = "NetChris/PublicBuildTesting"
+
+$AcrResourceId = $(az acr show --subscription $SubscriptionMain --name $AcrName --query "id" -o tsv)
+"ACR Resource Id: $AcrResourceId"
+$AcrLoginServer = $(az acr show --subscription $SubscriptionMain --name $AcrName --query "loginServer" -o tsv)
+"ACR LoginsServer: $AcrLoginServer"
+$CurrentSubscriptionId = $(az account show --query id -o tsv)
+"Current Subscription Id: $CurrentSubscriptionId"
+
+# Set the resource group name
+$ResourceGroup = "PublicBuildTesting"
+
+# Establish a trust relationship between Azure and GitHub Actions
+# Set the federated credential name
+$FederatedCredentialName = "$ManagedIdentityName-FC"
+
+# Set the GitHub branch name
+$GitHubBranch = "building-and-testing-dotnet"
+
+# Create the managed identity and return the service principal object id
+$ManagedIdentityObjectId = $(az identity create --subscription $CurrentSubscriptionId --resource-group $ResourceGroup --name $ManagedIdentityName --query principalId -o tsv)
+$ManagedIdentityClientId = $(az identity show   --subscription $CurrentSubscriptionId --resource-group $ResourceGroup --name $ManagedIdentityName --query clientId    -o tsv)
+$ManagedIdentityTenantId = $(az identity show   --subscription $CurrentSubscriptionId --resource-group $ResourceGroup --name $ManagedIdentityName --query tenantId    -o tsv)
+
+"Managed Identity ObjectId: $ManagedIdentityObjectId"
+"Managed Identity ClientId: $ManagedIdentityClientId"
+"Managed Identity TenantId: $ManagedIdentityTenantId"
+
+# Grant the managed identity access to ACR resource
+az role assignment create --role "AcrPush" --assignee-object-id $ManagedIdentityObjectId --assignee-principal-type ServicePrincipal --scope $AcrResourceId
+
+# TODO - I *think* there will be a FC required for each context in the repo which will need to talk to GH:
+# - main branch
+# - Each well-known branch?
+# - "Deployment" credentials for each deployment environment
+# See https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/about-security-hardening-with-openid-connect#example-subject-claims
+# See https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-azure
+# Create the federated credential
+az identity federated-credential create `
+  --name "${FederatedCredentialName}" `
+  --identity-name "${ManagedIdentityName}" `
+  --resource-group "${ResourceGroup}" `
+  --issuer https://token.actions.githubusercontent.com `
+  --subject repo:${GitHubRepo}:ref:refs/heads/${GitHubBranch}
+
+# TODO - Make sure to indicate the appropriate account/subscription and resource-group in the az call
+# GitHub ops
+# Get the client id and set the secret
+gh secret set AZURE_CLIENT_ID -b $ManagedIdentityClientId
+
+# TODO - Make sure to indicate the appropriate account/subscription and resource-group in the az call
+# Get the tenant id and set the secret
+gh secret set AZURE_TENANT_ID -b $ManagedIdentityTenantId
+
+# TODO - Make sure to indicate the appropriate account/subscription and resource-group in the az call
+# Get the subscription id and set the secret
+# Note, this must be a subscription where the managed identity has an role assignment.  It's unclear if it has to be the subscription
+# for the resources we're actually accessing.
+gh secret set AZURE_SUBSCRIPTION_ID -b $SubscriptionMain
+
+gh secret set ACR_LOGIN_SERVER -b $AcrLoginServer
