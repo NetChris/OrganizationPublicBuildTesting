fcf0b6d13a2f23df5ef109ebada15c5670845403
Chris Simmons <chris.dev@netchris.com>
2024-01-14 15:55:20 -0800

    Simple caller/called

--- scripts/op-candidate-clean.ps1
+++
@@ -1,2 +0,0 @@
-# Need to run from this dir
-op run --env-file="./script.env" -- pwsh ./candidate-clean.ps1
--- scripts/op-scratch1-clean.ps1
+++
@@ -1,2 +0,0 @@
-# Need to run from this dir
-op run --env-file="./script.env" -- pwsh ./scratch1-clean.ps1
